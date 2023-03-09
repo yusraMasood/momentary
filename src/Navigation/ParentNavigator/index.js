4ba3956af736e7af7b2ebe085f0a0adda7b3e754
yusraMasoodSalsoft4522 <yusra.masood@salsoft.net>
2023-03-09 16:55:34 +0500

    ios changes

--- src/Navigation/ParentNavigator/index.js
+++ src/Navigation/ParentNavigator/index.js
@@ -7,9 +7,9 @@ const ParentStack = createStackNavigator();
 
 const ParentNavigator = () => {
   return (
-    <ParentStack.Navigator initialRouteName='MainNavigator' screenOptions={{headerShown: false}}>
-        <ParentStack.Screen name="AuthNavigator" component={AuthNavigator} />
-        <ParentStack.Screen name="MainNavigator" component={MainNavigator} />
+    <ParentStack.Navigator screenOptions={{headerShown: false}}>
+      <ParentStack.Screen name="AuthNavigator" component={AuthNavigator} />
+      <ParentStack.Screen name="MainNavigator" component={MainNavigator} />
     </ParentStack.Navigator>
   );
 };
